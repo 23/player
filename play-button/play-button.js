db9db20d838eed426dfdc3d08c23a7689f151c42
Steffen Tiedemann Christensen <steffen@refresh.dk>
2012-06-24 16:49:32 +0200

    Play/pause button

--- play-button/play-button.js
+++ play-button/play-button.js
@@ -1,41 +1,33 @@
 /* 
  MODULE: PLAY BUTTON
- Show a play/pause/buffering button
+ Show a play/pause button
  
  Listens for:
- - video:play
- - video:playing 
- - video:pause 
- - video:ended
+ - player:video:play
+ - player:video:playing 
+ - player:video:pause 
+ - player:video:ended
 */
 
-Glue.provide('play-button', {}, 
-
+Glue.provide('play-button', 
+  {}, 
   function(Glue,$,opts){
-      var $this = this;
-      $.extend($this, opts);
-
-      $this.container.click(function(){
-          Glue.set('playing', !Glue.get('playing'));
+    var $this = this;
+    $.extend($this, opts);
+    $this.render();
+    
+    $this.container.click(function(e){
+        e.stopPropagation();
+        Glue.set('playing', !Glue.get('playing'));
+        $this.render();
+        return false;
       });
-          
-      $this.accessKey = 'p';
-      $this.tabIndex = true;
-      $this.className = 'player-play-paused';
-      var _update = function(e){
-          if(Glue.get('seeking')) {
-              $this.className = 'player-play-seeking';
-          } else if(Glue.get('playing')) {
-              $this.className = 'player-play-playing';
-          } else {
-              $this.className = 'player-play-paused';
-          }
-          
-          // re-render button
-      }
-      Glue.bind('video:play video:playing video:pause video:ended', _update);
+
+    Glue.bind('player:video:playing player:video:seeked player:video:pause player:video:ended', function(e){
+        $this.render();
+      });
       
-      return $this;
+    return $this;
   }
           
 );
