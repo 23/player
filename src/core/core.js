304e6d8971da9229adfcda7f7d6cad08a04a7484
steffentchr <steffentchr@Steffens-MacBook-Pro.local>
2013-01-02 23:07:58 +0100

    Fallback for domain

--- src/core/core.js
+++ src/core/core.js
@@ -108,8 +108,8 @@ Player.provide('core',
         });
 
       // Build domain
-      if($this.domain=='') $this.domain = document.domain;
-      if(document.domain=='localhost') $this.domain = 'reference.dev.visualtube.net';
+      if($this.domain=='') $this.domain = $this.settings.domain||document.domain;
+      if($this.domain=='localhost') $this.domain = 'reference.dev.visualtube.net';
       // Build player_id if we're loadin for examaple 1234.ithml
       if($this.player_id==0) {
           var p=location.pathname.match(/\/([0-9]+)\.i?html$/);
