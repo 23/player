dde3e9c9ab32b1539c32f6bd02bf388056f373ed
Steffen Tiedemann Christensen <steffen@tchr.dk>
2013-02-12 09:54:52 +0100

    Dedicated `playflow` module for prerolls, postrolls and an after video call-to-action.

---
+++ src/playflow/playflow.js
@@ -0,0 +1,210 @@
+/* 
+  MODULE: PLAYFLOW 
+
+  Fires for:
+  - player:playflow:video:start
+  - player:playflow:video:complete
+  - player:playflow:video:click
+  - player:playflow:video:close
+  - player:playflow:overlay:start
+  - player:playflow:overlay:complete
+  - player:playflow:overlay:click
+
+  Answers properties:
+  - closeIdentity [set]
+  - playflowActive [get]
+  - playflowState [get]
+  - playflowBeforeDownloadType [get]
+  - playflowBeforeDownloadURL [get]
+  - playflowBeforeLink [get]
+  - playflowAfterDownloadType [get]
+  - playflowAfterDownloadURL [get]
+  - playflowAfterLink [get]
+  - playflowAfterText [get]
+  - identityCountdown [get]
+  - identityAllowClose [get]
+  - playflowAdPosition [get]
+  - identityCountdownText [get]
+
+  Todo:
+  - flow
+  - fire events / check analytics
+  - update help texts in product
+  - 
+*/
+
+Player.provide('playflow', 
+  {
+    identityCountdown: false,
+    identityAllowClose: true,
+    identityCountdownTextSingular: "This advertisement will end in % second",
+    identityCountdownTextPlural: "This advertisement will end in % seconds"
+  }, 
+  function(Player,$,opts){
+      var $this = this;
+      $.extend($this, opts);
+      $this.playflowState = 'before'; // before, preroll, during, postroll, aftertext, ended
+      $this.playflowClip = '';
+      $this.playflowLink = '';
+
+      // Play either preroll or postroll clips
+      $this.beginClip = function(){
+        if(!$this.eingebaut||$this.playflowClip.length==0) return;
+        $this.eingebaut.setSource($this.playflowClip);
+        $this.eingebaut.setPlaying(true);
+        $($this.container).show();
+        $($this.canvas).show();
+        $($this.clicks).show();
+        $this.updateCountdown();
+      }
+      $this.endClip = function(){
+        $($this.container).hide();
+        $($this.canvas).hide();
+        $($this.clicks).hide();
+        if($this.eingebaut) $this.eingebaut.setPlaying(false);
+        if($this.playflowState=='before'||$this.playflowState=='preroll') {
+          $this.playflowState = 'during'
+          Player.set('playing', true);
+        } else if($this.playflowState=='during'||$this.playflowState=='postroll') {
+          $this.beginAfterText();
+        }
+        $this.updateCountdown();
+      }
+      $this.updateCountdown = function(){
+        if($this.playflowState=='preroll'||$this.playflowState=='postroll') {
+          $this.render(function(){}, 'playflow/playflow-countdown.liquid', $this.content);
+        } else {
+          $this.content.html('');
+        }
+      }
+
+      // Handle after text
+      $this.beginAfterText = function(){
+          $this.playflowState = 'aftertext';
+          if(Player.get('playflowAfterText').length>0) {
+              $($this.container).show();
+              $this.render(function(){}, 'playflow/playflow-after-text.liquid', $this.content);
+          } else {
+              $this.endAfterText();
+          }
+      }
+      $this.endAfterText = function(){
+          $($this.container).hide();
+          $this.content.html('');
+          $this.playflowState = 'ended';
+      }
+
+      // Create containers
+      $this.canvas = $(document.createElement('div')).addClass('playflow-canvas');
+      $this.container.append($this.canvas);
+      $this.clicks = $(document.createElement('div')).addClass('playflow-click-container');
+      $this.container.append($this.clicks);
+      $this.clicks.click(function(){
+          if($this.playflowLink.length>0) {
+            $this.endClip();
+            window.open($this.playflowLink);
+          }
+      });
+      $this.content = $(document.createElement('div')).addClass('playflow-content');
+      $this.container.append($this.content);
+    
+      // Logic to load the display device with Eingebaut
+      $this.eingebaut = null;
+      $this.loadEingebaut = function(){
+          if($this.eingebaut) return;
+
+          $this.eingebaut = new Eingebaut($this.canvas, Player.get('displayDevice'), '', function(e){
+              // Error if no display device is available
+              if(e=='loaded'&&$this.eingebaut.displayDevice=='none') {
+                $this.cancelPlayflow();
+              }
+              // If this loads after the content (i.e. if we're switching display device, fire an event that we're ready)
+              if(e=='loaded') {
+                $this.beginClip();
+              } else if(e=='ended') {
+                Player.fire('player:playflow:video:complete');
+                $this.endClip();
+              } else if(e=='progress'||e=='timeupdate') {
+                $this.updateCountdown();
+              }
+            });
+        $this.eingebaut.load();
+      };
+
+      // Merge in player settings and update the display if needed
+      Player.bind('player:settings', function(){
+          PlayerUtilities.mergeSettings($this, ['identityCountdown', 'identityAllowClose', 'identityCountdownTextSingular', 'identityCountdownTextPlural']);
+      });
+
+      // Play advertising on play, if any
+      $this.initiateClip = function(type) {
+          $this.playflowClip = '';
+          $this.playflowLink = '';
+          var playflow_type = Player.get(type=='preroll' ? 'playflowBeforeDownloadType' : 'playflowAfterDownloadType');
+          var url = Player.get(type=='preroll' ? 'playflowBeforeDownloadURL' : 'playflowAfterDownloadURL');
+          if(playflow_type=='video' && url.length>0) {
+            $this.loadEingebaut();
+            $this.playflowState = type;
+            $this.playflowClip = Player.get('url') + url;
+            $this.playflowLink = Player.get(type=='preroll' ? 'playflowBeforeLink' : 'playflowAfterLink')
+            $this.beginClip();
+            return false;
+          } else {
+            $this.endClip();
+            return true;
+          }
+      }
+      Player.bind('player:video:beforeplay', function(){
+          if($this.playflowState!='before') return true;
+          return($this.initiateClip('preroll'));
+      });
+      Player.bind('player:video:ended', function(){
+          return($this.initiateClip('postroll'));
+      });
+      Player.bind('player:video:loaded', function(){
+          $this.playflowState = 'before';
+      });
+
+      // Close clip
+      Player.setter('closeIdentity', function(){
+          if($this.playflowState=='aftertext') {
+              $this.endAfterText();
+          } else if(Player.get('identityAllowClose')) {
+              Player.fire('player:playflow:video:close');
+              $this.endClip();
+          }
+      });
+
+      // Expose properties
+      Player.getter('playflowBeforeDownloadType', function(){var v = Player.get('video'); return (v&&v.before_download_type ? v.before_download_type||'' : '');});
+      Player.getter('playflowBeforeDownloadURL', function(){var v = Player.get('video'); return (v&&v.before_download_url ? v.before_download_url||'' : '');});
+      Player.getter('playflowBeforeLink', function(){var v = Player.get('video'); return (v&&v.before_link ? v.before_link||'' : '');});
+
+      Player.getter('playflowAfterDownloadType', function(){var v = Player.get('video'); return (v&&v.after_download_type ? v.after_download_type||'' : '');});
+      Player.getter('playflowAfterDownloadURL', function(){var v = Player.get('video'); return (v&&v.after_download_url ? v.after_download_url||'' : '');});
+      Player.getter('playflowAfterLink', function(){var v = Player.get('video'); return (v&&v.after_link ? v.after_link||'' : '');});
+
+      Player.getter('playflowAfterText', function(){var v = Player.get('video'); return (v&&v.after_text ? v.after_text||'' : '');});
+
+      Player.getter('playflowActive', function(){return $this.playflowState=='preroll'||$this.playflowState=='postroll'||$this.playflowState=='aftertext';});
+      Player.getter('playflowState', function(){return $this.playflowState;});
+      Player.getter('identityCountdown', function(){return $this.identityCountdown;});
+      Player.getter('identityAllowClose', function(){return $this.identityAllowClose;});
+      Player.getter('playflowAdPosition', function(){return ($this.playflowState=='preroll'||$this.playflowState=='postroll'||$this.playflowState=='aftertext' ? $this.playflowState : 'none');});
+
+      Player.getter('identityCountdownText', function(){
+        try {
+          var duration = $this.eingebaut.getDuration();
+          var currentTime = $this.eingebaut.getCurrentTime();
+          var timeLeft = Math.round(duration-currentTime);
+          if(isNaN(timeLeft)) return '';
+          var s = (timeLeft==1 ? $this.identityCountdownTextSingular : $this.identityCountdownTextPlural);
+          return s.replace(/\%/img, timeLeft);
+        }catch(e){
+          return '';
+        }
+      });
+      
+      return $this;
+  }
+);
