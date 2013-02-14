70787996613f7b7dd2ff78a0064855df065a5215
Kalle Kabell <kkabell@gmail.com>
2013-02-14 08:50:56 +0100

    Volume control

--- src/volume-button/volume-button.js
+++ src/volume-button/volume-button.js
@@ -11,11 +11,65 @@ Player.provide('volume-button',
   function(Player,$,opts){
     var $this = this;
     $.extend($this, opts);
-    $this.render();
-    
-    Player.bind('player:video:volumechange', function(e){
-        $this.render();
+    $this.videoVolume = null;
+    $this.render(function(){
+        $this.volumeHandle = $this.container.find('.volume-handle');
+
+        /* Make volume-handle draggable */
+
+        if($this.volumeHandle) {
+          $('body').on('mousedown', '.volume-handle', function(e){
+            // Enable dragging and different positioning of the volume handle
+            $this.videoVolume = Player.get('volume'); 
+            e.stopPropagation();
+          });
+          $(document).mousemove(function(e){
+              if($this.videoVolume!==null) {
+                  $this.volumeHandle = $this.container.find('.volume-handle');
+                  $this.volumeFilled = $this.container.find('.volume-filled');
+                  $this.volumeTrack = $this.container.find('.volume-track');
+
+                  var trackTop = $this.volumeTrack.offset()['top'];
+                  var trackHeight = $this.volumeTrack.height();
+                  var volumeFilledHeight = Math.max(0, Math.min(trackHeight - (e.clientY-trackTop), trackHeight));
+                  $this.volumeFilled.height(volumeFilledHeight);
+                  $this.volumeHandle.css({bottom: (volumeFilledHeight+4)});
+                  var volume = volumeFilledHeight / trackHeight;
+                  Player.set('volume', volume);
+              }
+          });
+          $(document).mouseup(function(e){
+              if($this.videoVolume!==null) {
+                  $this.videoVolume = null;
+                  e.stopPropagation();
+              }
+          });
+          $this.volumeHandle.on('click', function(e){
+              // Clicks on the handle shouldn't bubble to clicks on the scrubber
+              e.stopPropagation(); 
+          });
+        }
+    });
+
+    var updateVolume = function() {
+      var volume = Player.get('volume');
+      $this.volumeHandle = $this.container.find('.volume-handle');
+      $this.volumeFilled = $this.container.find('.volume-filled');
+      $this.volumeTrack = $this.container.find('.volume-track');
+
+      var volumeFilledHeight = (parseInt($this.volumeTrack.parent().css('height')) - 18) * volume;
+      //var volumeFilledHeight = $this.volumeTrack.height() * volume;
+      $this.volumeFilled.css({height: volumeFilledHeight});
+      $this.volumeHandle.css({bottom: Math.max(volumeFilledHeight+4, 4)});
+    }
+
+    Player.bind('player:video:volumechange player:load', function(e){
+        if($this.videoVolume==null) {
+          updateVolume();
+        }
       });
+
+    Player.bind('player:volume-engaged', function(e){updateVolume();});
 
     /* GETTERS */
     Player.getter('volumeMuted', function(){return (Player.get('volume')==0);});
