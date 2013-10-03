7ea541069c3b6155af691e3879953ce49777b759
Kalle Kabell <kkabell@gmail.com>
2013-10-03 17:44:05 +0200

    Quicker response on touch events

--- src/design/design.js
+++ src/design/design.js
@@ -117,12 +117,18 @@ Player.provide('design',
           $('video-display').css({bottom:$this.verticalPadding+'px', left:$this.horizontalPadding+'px'})
       }
 
-      // Fullscreen on pinch to zoom
       if ('ontouchstart' in document.documentElement) {
+          // Fullscreen on pinch to zoom
           $(document).on("gesturechange", function(e){
               if (e.originalEvent.scale > 1) {
 		  Player.set("fullscreen", true);
               }
+          });
+          // Quicker response on tap
+          $(document).on("touchstart", function(e){
+              $(e.target).click();
+              e.preventDefault();
+              e.stopPropagation();
           });
       }
 
