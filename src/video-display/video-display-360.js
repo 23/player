bdcad3290ded08e01aad85354bfe182b9f01d6cb
Kalle Kabell <kkabell@gmail.com>
2016-07-06 17:26:01 +0200

    Add 'CustomEvent' polyfill for 360 support in IE11

--- src/video-display/video-display-360.js
+++ src/video-display/video-display-360.js
@@ -114,3 +114,25 @@ var ThreeSixtyController = (function(){
         displaying360: _displaying360
     };
 })();
+
+// Polyfill for 'CustomEvent' used by Aframe
+// Needed for 360 support in IE 11
+// https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent/CustomEvent#Polyfill
+(function () {
+    try {
+
+        if ( typeof window.CustomEvent === "function" ) return false;
+
+        function CustomEvent ( event, params ) {
+            params = params || { bubbles: false, cancelable: false, detail: undefined };
+            var evt = document.createEvent( 'CustomEvent' );
+            evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+            return evt;
+        }
+
+        CustomEvent.prototype = window.Event.prototype;
+
+        window.CustomEvent = CustomEvent;
+        
+    }catch(e){}
+})();
